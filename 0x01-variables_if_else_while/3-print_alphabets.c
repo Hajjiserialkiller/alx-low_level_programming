54504eab879949c32498080da2c445ae1ccd1ddb
Ahmed Hajji <fbidea4b@gmail.com>
2023-03-17 19:15:45 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-char apl[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+char apl[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i;
 for (i = 0, i < 52, i++)
 {
-putchar(alp[i])
+putchar(alp[i]);
 }
 putchar('\n');
 return (0);
